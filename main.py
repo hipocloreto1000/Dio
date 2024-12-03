046b0ea6a71a8ef9387f92d0fda9ad0ebb8c4bd2
hipocloreto1000 <122793823+hipocloreto1000@users.noreply.github.com>
2024-12-03 19:08:33 +0000

    git status

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 #Estrututra Condicional Simples
 
-soma = 3
+#soma = 3
 
-if soma > 0:
-    print("Maior que zero")
+#if soma > 0:
+  #  print("Maior que zero")
   
-else:
-    print("menor que zero")
+#else:
+    #print("menor que zero")
+
+#fruit = ["apple","banana","cherry"]
+#x,y,z = fruit
+#print(x)
+#print(y)
+#print(z)
+
+#n1 = int(input('Digite um número: '))
+#n2 = int(input('Digite mais um número: '))
+#s = n1 + n2
+#print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+#print(type(n1), type(n2))
+
+# Para verificar se a entrada é numérica, faça isso antes da conversão:
+#n1_str = input('Digite um número: ')
+#n2_str = input('Digite mais um número: ')
+
+#if n1_str.isnumeric() and n2_str.isnumeric():
+ #   n1 = int(n1_str)
+  #  n2 = int(n2_str)
+   # s = n1 + n2
+    #print('A soma entre {} e {} vale  {}'.format(n1, n2, s))
+#    print(type(n1), type(n2))
+#else:
+#    print("Por favor, insira apenas números.")
+
+# Verificação de alfabético para strings
+#print(n1_str.isalpha(), n2_str.isalpha())
+
